98167f81e3a9165a7649a0519acfa50d4d0ef07d
Rohit <raggarwal300@gmail.com>
2022-11-12 10:20:33 +0530

    Used general xpath

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -6,46 +5,46 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 import time
+import openpyxl
 
 service = Service(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
 wait = WebDriverWait(driver, 20)
 
-driver.get('SITE/login')
+wb = openpyxl.load_workbook("data/data.xlsx")
+ws = wb['Sheet1']
+rows = ws.max_row + 1
+for i in range(2, rows):
+    idx = ws.cell(row=i, column=1).value
+    passx = ws.cell(row=i, column=2).value
+    dmcx = ws.cell(row=i, column=3).value
 
-userid = driver.find_element(By.ID, 'username')
-userid.send_keys('USERID')
+    driver.get('SITE/login')
 
-password = driver.find_element(By.ID, 'password')
-password.send_keys('PASSWORD')
+    userid = driver.find_element(By.ID, 'username')
+    userid.send_keys(idx)
 
-signin = driver.find_element(By.XPATH, '//*[@id="root"]/main/div[2]/div/form/button')
-signin.click()
+    password = driver.find_element(By.ID, 'password')
+    password.send_keys(passx)
 
-wait.until(ec.title_is("Home Page"))
-driver.get("SITE/Webportal/Documents")
+    signin = driver.find_element(By.XPATH, '//*[@id="root"]/main/div[2]/div/form/button')
+    signin.click()
 
-wait.until(ec.element_to_be_clickable((By.XPATH, '//*[@id="DMC"]/div[1]'))).click()
+    wait.until(ec.title_is("Home Page"))
+    driver.get("SITE/Webportal/Documents")
 
-data = []
+    wait.until(ec.element_to_be_clickable((By.XPATH, '//*[@id="DMC"]/div[1]'))).click()
 
-with open('data/status.csv', 'r') as status:
-    sta = csv.reader(status)
-    for row in sta:
-        data.append(row[0])
+    download = driver.find_elements(By.XPATH, "//div[@id='DMC-content']//div//div//div//div[1]//div[1]//div[1]//div[2]//span[2]//*[name()='svg']")
+    time.sleep(1)
+    m = len(download)
+    o = m - dmcx
 
-with open('data/status.csv', 'a') as addition:
-    add = csv.writer(addition)
+    for j in range(0, o):
+        download[j].click()
+        time.sleep(10)
+        dmcx += 1
+        ws.cell(row=i, column=3).value = dmcx
 
-    with open('data/data.csv', 'r') as csv_file:
-        csv_reader = csv.reader(csv_file)
-
-        for line in csv_reader:
-
-            if line[0] not in data:
-                download = driver.find_element(By.XPATH, line[1])
-                time.sleep(2)
-                download.click()
-                add.writerow([line[0]])
-                time.sleep(5)
+wb.save("data/data.xlsx")
